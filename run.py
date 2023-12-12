5fcca440152446f66f2c058763724fab441ab9b8
vivianlin24 <81603337+vivianlin24@users.noreply.github.com>
2023-12-11 21:56:30 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,47 @@
+%load_ext autoreload
+%autoreload 2
+
+import sys
+import subprocess
+
+import pandas as pd 
+from matplotlib import pyplot as plt
+from collections import Counter
+import numpy as np
+import openai
+from langchain.llms import OpenAI
+import random
+import regex as re
+from langdetect import detect
+import seaborn as sns
+import matplotlib.pyplot as plt
 from json import load
+
+def install_packages():
+    try:
+        dependencies = ['numpy', 'pandas', 'matplotlib', 'OpenAI', 'langchain']
+        for package in dependencies:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+        print("Packages installed successfully.")
+    except Exception as e:
+        print(f"Error: {e}")
 
 filenames = ["demo.ipynb",
             "exploratory_visualizations.ipynb",
             "agent_responses.ipynb", 
             "agent_response_visualizations.ipynb", 
             "final_bias_visualizations.ipynb"]
-for filename in filenames:
-    with open(filename) as fp:
-        nb = load(fp)
 
-    for cell in nb['cells']:
-        if cell['cell_type'] == 'code':
-            source = ''.join(line for line in cell['source'] if not line.startswith('%'))
-            exec(source, globals(), locals())
+    
+if __name__ == '__main__':
+    install_packages()
+    args = sys.argv[1:]
+    if 'data' in args:
+        for filename in filenames:
+            with open(filename) as fp:
+                nb = load(fp)
+
+            for cell in nb['cells']:
+                if cell['cell_type'] == 'code':
+                    source = ''.join(line for line in cell['source'] if not line.startswith('%'))
+                    exec(source, globals(), locals())
